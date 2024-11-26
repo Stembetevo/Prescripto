f64574443cc99c95b3078ef1a5ff70a0cbaf5322
Steka <stephenkaruru05@gmail.com>
2024-11-26 03:46:52 +0300

    Filtering doctors with their specialties

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { AppContext } from '../context/AppContext';
+import { doctors } from '../assets/assets_frontend/assets';
 
 
 const TopDoctors = () => {
     
     const navigate = useNavigate();
-    const {doctors} = useContext(AppContext)
+    
 
 
     return (
@@ -14,7 +14,8 @@ const TopDoctors = () => {
         <h1 className='text-3xl font-medium'>Our Doctors</h1>
         <p className='sm:w-1/3 text-center text-sm'>Browse through our doctors who meets your attention.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-            {doctors.slice(0,10).map((item,index)=>(
+            {
+            doctors.slice(0,10).map((item,index)=>(
                 <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[10px] transition-all duration-500' key={index}>
                 <img onClick = {()=>navigate(`/appointment/${item._id}`)}className='bg-blue-50' src={item.image} alt="" />
                 <div className='pt-4'>
